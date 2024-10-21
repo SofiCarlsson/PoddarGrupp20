004fbfbcd20cd4774f01bf954e5f81d8d1853ecf
Sofi <carlssonsofi99@gmail.com>
2024-10-21 18:10:50 +0200

    fix

--- PoddarGrupp20/PoddarGrupp20/obj/Debug/net8.0-windows/PoddarGrupp20.AssemblyInfo.cs
+++ PoddarGrupp20/PoddarGrupp20/obj/Debug/net8.0-windows/PoddarGrupp20.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PoddarGrupp20")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea090d32bc5657cc8d4fd76aa7448351aa7a0fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("PoddarGrupp20")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PoddarGrupp20")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
